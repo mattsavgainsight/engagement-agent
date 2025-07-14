5cda79412cba082f11a50710964fd730b11f0b37
mattsavgainsight <msavelloni@gainsight.com>
2025-07-14 09:48:39 -0400

    Delete pages/pages directory

--- pages/pages/admin.js
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold">Admin Dashboard</h1>
-      <p className="mt-4">View engagement data and take actions here.</p>
-    </div>
-  )
-}
--- pages/pages/chat.js
+++
@@ -1,8 +0,0 @@
-export default function Chat() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold">Chat Agent</h1>
-      <p className="mt-4">This is the agent chat interface.</p>
-    </div>
-  )
-}
