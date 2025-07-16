1adc89552c6d493d60b4c5cb4121298fa83d5b05
mattsavgainsight <msavelloni@gainsight.com>
2025-07-16 15:24:14 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,16 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,jsx,ts,tsx}', 
-    './components/**/*.{js,jsx,ts,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        skilljar: {
-          blue: '#0052CC',
-          darkblue: '#003D99',
-          lightblue: '#E6F0FF',
-          redlight: '#FEE2E2',
-          reddark: '#B91C1C',
-          greenlight: '#D1FAE5',
-          greendark: '#065F46',
-        },
-      },
-      fontFamily: {
-        sans: ['"Helvetica Neue"', 'Arial', 'sans-serif'],
-      },
-    },
+        'skilljar-dark': '#003d99',
+        'skilljar-light': '#f4f8fc'
+      }
+    }
   },
   plugins: [],
-};
+}
